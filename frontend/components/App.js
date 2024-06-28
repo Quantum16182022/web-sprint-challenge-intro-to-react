764548f2de79a6a5a75e535c546b87029f8b2664
Stanley kemboi <kstan8482@gmail.com>
2024-06-28 12:41:50 -0500

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,30 +2,52 @@ import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
+
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
-  useEffect(() => {
-    function fetchPhoto(){
-      axios.get(URL)
-            .then(res => {
-              console.log(res.data)
-            })
-            .catch(err => {
-              console.log(err.message)
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
 
-            })
-           }
-           fetchPhoto
-  }, [])
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      try {
+        const res = await axios.get(urlPeople);
+        setCharacters(res.data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+    fetchCharacters();
+  }, []);
+
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      try {
+        const res = await axios.get(urlPlanets);
+        setPlanets(res.data);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+    fetchPlanets();
+  }, []);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map((character, index) => (
+        <Character key={index} character={character} />
+      ))}
+      <h2>Star Wars Planets</h2>
+      {planets.map((planet, index) => (
+        <div key={index}>
+          <h3>{planet.name}</h3>
+          <p>{planet.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
