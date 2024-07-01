95d4bc2c568bf3f85af6913a016f36b440dc649f
Stanley kemboi <kstan8482@gmail.com>
2024-07-01 13:16:25 -0500

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,54 +6,115 @@ const urlPlanets = 'http://localhost:9009/api/planets';
 const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
+
   const [characters, setCharacters] = useState([]);
   const [planets, setPlanets] = useState([]);
-  const [clickedCharacter, setClickedCharacter] = useState(null);
+
   useEffect(() => {
     const fetchCharacters = async () => {
       const [planetsData, peopleData] = await Promise.all([
-        axios.get(urlPlanets),    
-        axios.get(urlPeople)        
+        axios.get(urlPlanets),
+        axios.get(urlPeople)
       ]);
-      console.log(planetsData, peopleData)
-      setCharacters(peopleData.data);
+
       setPlanets(planetsData.data);
+
+      setCharacters(peopleData.data.map(character => ({
+        ...character,
+        planets: character.planets
+          ? character.planets.map(planetId => {
+              const planet = planetsData.data.find(planet => planet.id === planetId);
+              return planet ? planet.name : 'Unknown Planet';
+            })
+          : [],
+      })));
     };
     fetchCharacters();
   }, []);
-
-  // characters = characters.map(character => {
-  //   character.planet = character.planets.map(planetId => {
-  //     const planet = planet.find(planet => planet.id === planetId);
-  //     return planet.name;
-  //   });
-  //   return character;
-  // });
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {characters.map((character, index) => {
-  // Find the planet associated with the current character
-  const characterPlanet = planets.find(planet => planet.id === planet.name);
-
-  return (
-    <Character
-  key={index}
-  character={character}
-  onClick={() => setClickedCharacter(clickedCharacter === character.id ? null : character.id)}
-  // The planet name is only passed if this character has been clicked
-  planet={clickedCharacter === character.id && (characterPlanet ?characterPlanet.name : 'Unknown Planet')}
-/>
-   
-  );
-})}
-console.log(Character)
+      {characters.map((character, index) => (
+        <Character
+          key={index}
+          character={character}
+          planet={character.planets.join(', ')}
+        />
+      ))}
     </div>
   );
 }
 
 export default App;
-// ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = App
+
+// :exclamation: DO NOT CHANGE THE CODE BELOW
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = App;
+}
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Character from './Character';
+
+// const urlPlanets = 'http://localhost:9009/api/planets';
+// const urlPeople = 'http://localhost:9009/api/people';
+
+// function App() {
+  
+//   const [characters, setCharacters] = useState([]);
+//   const [planets, setPlanets] = useState([]);
+
+  
+//   useEffect(() => {
+//     const fetchCharacters = async () => {
+//       const [planetsData, peopleData] = await Promise.all([
+//         axios.get(urlPlanets),    
+//         axios.get(urlPeople)        
+//       ]);
+  
+//       setPlanets(planetsData.data);
+  
+//       setCharacters(peopleData.data.map(character => ({
+//         ...character,
+//         planets: character.planets
+//           ? character.planets.map(planetId => {
+//               const planet = planetsData.data.find(planet => planet.id === planetId);
+//               return planet ? planet.name : 'Unknown Planet';
+//             })
+//           : [],
+//       })));
+//     };
+//     fetchCharacters();
+//   }, []);
+
+//   function Character({ character, planet }) {
+//   const [showPlanet, setShowPlanet] = useState(false);
+ 
+
+//   return (
+    
+//      <div className="card">
+//       <h3>{character.name}</h3>
+//       <button onClick={() => setShowPlanet(!showPlanet)}>
+//         {showPlanet ? 'Hide Planet' : 'Show Planet'}
+//       </button>
+//       <h2>Star Wars Characters</h2>
+//       <p>See the README of the project for instructions on completing this challenge</p>
+//       {characters.map((character, index) => (
+//         <Character
+//           key={index}
+//           character={character}
+//           planet={character.planets.join(', ')}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// // ❗ DO NOT CHANGE THE CODE  BELOW
+//   if (typeof module !== 'undefined' && module.exports) module.exports = App}
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,21 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
-  function Character(props) {
-    const [showHomeworld, setShowHomeworld] = useState(false);
+function Character({ character, planet }) {
+  const [showPlanet, setShowPlanet] = useState(false);
+
+  return (
+    <div className="card">
+      <h3>{character.name}</h3>
+      <button onClick={() => setShowPlanet(!showPlanet)}>
+        {showPlanet ? 'Hide Planet' : 'Show Planet'}
+      </button>
+      {showPlanet && (
+        <>
+          <h4>Planet</h4>
+          <p>{planet}</p>
+        </>
+      )}
+    </div>
+  );
+}
+export default Character;
+// import React from 'react';
+//   function Character({ character, planet }) {
+//     const [showPlanet, setShowPlanet] = useState(false);
   
-    const toggleHomeworld = () => {
-      setShowHomeworld(!showHomeworld);
-    };
-  
-    return (
-      <div className="character-card" onClick={toggleHomeworld}>
-        <h3 className="character-name">{props.character.name}</h3>
-        <p>{props.character.description}</p>
-        {showHomeworld && <p className="character-planet">{props.planet}</p>}
-      </div>
-    )
-  }  
-  
-export default Character
+//   return (
+//      <div className="card">
+//       <h3>{character.name}</h3>
+//       <button onClick={() => setShowPlanet(!showPlanet)}>
+//         {showPlanet ? 'Hide Planet' : 'Show Planet'}
+//       </button>
+//       <h4>Planet</h4>
+//       <p>{planet}</p>
+//     </div>
+//   );
+// }
+// export default Character;
